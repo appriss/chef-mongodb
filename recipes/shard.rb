a8a161e7d8a88007a8f65981edca60f7769ecf04
Donavan Pantke <dpantke@appriss.com>
2014-08-14 12:23:36 -0400

    Debugging.

--- recipes/shard.rb
+++ recipes/shard.rb
@@ -34,6 +34,7 @@ mongodb_instance node['mongodb']['instance_name'] do
   port         node['mongodb']['config']['port']
   logpath      node['mongodb']['config']['logpath']
   dbpath       node['mongodb']['config']['dbpath']
+  Chef::Log.info("Are we a replicaset? #{node['mongodb']['is_replicaset']}")
   replicaset   node if node['mongodb']['is_replicaset']
   enable_rest  node['mongodb']['config']['rest']
   smallfiles   node['mongodb']['config']['smallfiles']
